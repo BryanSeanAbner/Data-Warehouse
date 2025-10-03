fda2daa0bb47e2179d802393520e699752c1facf
TimothyRT <timothyrtzion@gmail.com>
2025-10-03 23:46:48 +0700

    Implemented logic for DateDimension's Holiday Indicator

--- database/seeders/DateDimensionSeeder.php
+++ database/seeders/DateDimensionSeeder.php
@@ -3,10 +3,12 @@
 namespace Database\Seeders;
 
 use Carbon\Carbon;
+use Pharaonic\Hijri\HijriCarbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+
+Carbon::mixin(HijriCarbon::class); 
 
 class DateDimensionSeeder extends Seeder
 {
@@ -44,7 +46,10 @@ class DateDimensionSeeder extends Seeder
                 'fiscal_quarter' => $calendarQuarter,
                 'fiscal_year_quarter' => $start->format('Y-') . $calendarQuarter,
                 'fiscal_year' => $start->year,
-                'holiday_indicator' => 'Non-Holiday',
+                'holiday_indicator' => match ($this->isHoliday($start)) {
+                    true => "Holiday",
+                    false => "Non-holiday"
+                },
                 'weekday_indicator' => match ($start->isWeekday()) {
                     true => "Weekday",
                     false => "Weekend",
@@ -55,5 +60,52 @@ class DateDimensionSeeder extends Seeder
 
         DB::table('date_dimension')->insert($dates);
 
+    }
+
+    /**
+     * Return true if the Carbon instance provided is considered a holiday and false otherwise.
+     */
+    private function isHoliday(Carbon $date): bool {
+        $year = $date->year;
+        $monthDay = (string)$date->format('m-d');
+
+        $isFixedHoliday = match ($monthDay) {
+            '01-01' => true, // New Year
+            '05-01' => true, // Labor Day
+            '06-01' => true, // Pancasila Day
+            '08-17' => true, // Independence Day
+            '12-25' => true, // Christmas
+            default => false,
+        };
+        if ($isFixedHoliday) return true;
+
+        $monthDayHijri = (string)$date->toHijri()->format('m-d');
+
+        $isIslamicHoliday = match ($monthDayHijri) {
+            '01-01' => true, // Islamic New Year
+            '03-12' => true, // Prophet Muhammad's Birthday
+            '07-27' => true, // Isra Mi'raj
+            '10-01' => true, // Eid al-Fitr
+            '12-10' => true, // Eid al-Adha
+            default => false,
+        };
+        if ($isIslamicHoliday) return true;
+
+        $easterSunday = Carbon::createFromTimestamp(
+            easter_date($year),
+            new \DateTimeZone('Asia/Jakarta'));
+        Log:info($easterSunday);
+        $goodFriday = $easterSunday->copy()->subDays(2); // 2 days before Easter
+        $ascensionDay = $easterSunday->copy()->addDays(39); // 39 days after Easter
+        if ($date->isSameDay($easterSunday)) return true;
+        if ($date->isSameDay($goodFriday)) return true;
+        if ($date->isSameDay($ascensionDay)) return true;
+        
+        // Unimplemented:
+            // Chinese New Year
+            // Balinese Day of Silence (Nyepi)
+            // Vesak Day
+
+        return false;
     }
 }
